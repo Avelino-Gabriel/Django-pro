ee0e7aa6d920adabacb5d1e49b28327159b84474
Avelino-Gabriel <aluno103.23187221gabriel@gmail.com>
2024-03-19 22:02:04 -0300

    Allowed Hosts

--- django_senai/settings.py
+++ django_senai/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-b1ovvo0mk1^--bum+bfod2klt#v1)!+=d%!rvq)4ctlec03$08
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+'GabrielAvelino.pythonanywhere.com'
+]
 
 
 # Application definition
